ab55e327be1dbff00659222057c9d2ed1660c292
Adam Feuer <adam@adamfeuer.com>
2020-03-11 12:56:29 -0700

    added --follow to git log
    - to ensure previous names are checked too

--- tools/license-clearing/authors.sh
+++ tools/license-clearing/authors.sh
@@ -4,8 +4,8 @@ REPORT="$FILE.authors"
 
 if [ "$FILE" = "-h" ]; then
     echo "Usage: $0 [FILE-PATHNAME]"
-    echo "  creates FILE-PATHNAME.authors file with sorted list of unique author names and email addresses from git log"
+    echo "  Creates FILE-PATHNAME.authors file with sorted list of unique author names and email addresses from git log."
 fi
 
 echo $FILE > $REPORT  
-git log "$FILE" | grep Author | sort | uniq | sed 's/Author://g' >> $REPORT
+git log --follow "$FILE" | grep Author | sort | uniq | sed 's/Author://g' >> $REPORT
