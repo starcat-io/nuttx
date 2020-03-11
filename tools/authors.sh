2bea0bdc256cf6ed96e691ae1cb53b04c31bae77
Adam Feuer <adam@adamfeuer.com>
2020-03-11 09:51:01 -0700

    initial addition

---
+++ tools/authors.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+FILE=$1
+REPORT="$FILE.authors"
+
+if [ "$FILE" = "-h" ]; then
+    echo "Usage: $0 [FILE-PATHNAME]"
+    echo "  creates FILE-PATHNAME.authors file with sorted list of unique author names and email addresses from git log"
+fi
+
+echo $FILE > $REPORT  
+git log "$FILE" | grep Author | sort | uniq | sed 's/Author://g' >> $REPORT
